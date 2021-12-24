5d8a901819827e6841089db32ab0283810728149
Alejandro Henestroza <alehenestroza@gmail.com>
2021-12-24 15:44:46 -0300

    Added buttons to switch from 24-hr format to 12-hr (#1)

--- js/app.js
+++ js/app.js
@@ -1,26 +1,41 @@
 const clock = document.getElementById('clock');
 const date = document.getElementById('date');
-// const btn24hr = document.getElementById('24hr');
-// const btn12hr = document.getElementById('12hr');
+const btn24hr = document.getElementById('24hr');
+const btn12hr = document.getElementById('12hr');
 const colorpicker = document.getElementById('colorpicker');
+let is24hr = true;
 
-// btn12hr.addEventListener('click', () => {
-// 	// TODO: Implement 12hr mode
-// });
-// btn24hr.addEventListener('click', () => {
-// 	// TODO: Implement 24hr mode
-// });
+btn12hr.addEventListener('click', () => {
+	is24hr = false;
+});
+btn24hr.addEventListener('click', () => {
+	is24hr = true;
+});
 colorpicker.addEventListener('change', e => {
 	const color = e.target.value;
 	document.body.style.backgroundColor = color;
 });
 
+const convertTo12hr = hours => {
+	if (hours > 12) {
+		return hours - 12;
+	} else if (hours === 0) {
+		return 12;
+	}
+	return hours;
+};
+
 // Get the current time in the client's timezone, will return an array of [hours, hours, minutes, minutes, seconds, seconds].
 const getTime = () => {
 	const dt = new Date();
-	const hours = dt.getHours();
+	let hours = dt.getHours();
 	const minutes = dt.getMinutes();
 	const seconds = dt.getSeconds();
+	let ampm = hours >= 12 ? 'pm' : 'am';
+
+	if (!is24hr) {
+		hours = convertTo12hr(hours);
+	}
 
 	const time = [];
 
@@ -51,6 +66,8 @@ const getTime = () => {
 		time.push(...rest);
 	}
 
+	if (!is24hr) time.push(ampm);
+
 	return time;
 };
 
@@ -59,15 +76,22 @@ const printTime = () => {
 	const time = getTime();
 
 	time.forEach((element, index) => {
-		if (index === 2 || index === 4) {
-			const colon = document.createElement('span');
-			colon.innerHTML = ':';
-			clock.appendChild(colon);
+		if (index === 6) {
+			const ampm = document.createElement('span');
+			ampm.classList.add('clock__time-ampm');
+			ampm.innerHTML = element;
+			clock.appendChild(ampm);
+		} else {
+			if (index === 2 || index === 4) {
+				const colon = document.createElement('span');
+				colon.innerHTML = ':';
+				clock.appendChild(colon);
+			}
+			const div = document.createElement('div');
+			div.classList.add('clock__time-digit');
+			div.innerText = element;
+			clock.appendChild(div);
 		}
-		const div = document.createElement('div');
-		div.classList.add('clock__time-digit');
-		div.innerText = element;
-		clock.appendChild(div);
 	});
 };
 
